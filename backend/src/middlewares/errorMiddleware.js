db4edfbbe70318c27a16b513a87f5b0ea165e5f2
Marwan Salem <marwansalem164@gmail.com>
2026-01-12 06:18:09 +0200

    Implement global error handling middleware

---
+++ backend/src/middlewares/errorMiddleware.js
@@ -0,0 +1,32 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    // Mongoose duplicate key error
+    if(err.code === 11000) {
+        statusCode = 400;
+        const field = Object.keys(err.keyValue)[0];
+        message = `The ${field} '${err.keyValue[field]}' is already in use. Please choose another one.`;
+
+        err = new ErrorHandler(message, statusCode);
+    }
+    
+    // Mongoose validation error
+    if(err.name === "ValidationError") {
+        statusCode = 400;
+        const messages = Object.values(err.errors).map(value => value.message);
+        message = `${messages}`;
+
+        err = new ErrorHandler(message, statusCode);
+    }
+
+    console.log(err);
+
+    if (statusCode >= 400 && statusCode < 500) {
+        res.fail(message, statusCode)
+    } else {
+        res.error(message, statusCode)
+    }
+}
