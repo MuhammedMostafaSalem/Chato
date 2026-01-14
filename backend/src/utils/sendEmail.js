7023b907427b6375cec0a4b9bf88dff323ed285a
Marwan Salem <marwansalem164@gmail.com>
2026-01-14 07:28:53 +0200

    Add nodemailer configuration for sending emails

---
+++ backend/src/utils/sendEmail.js
@@ -0,0 +1,28 @@
+const nodeMailer = require('nodemailer');
+
+// Function to send email
+const sendEmail = async (options) => {
+    // Create a transporter
+    const transporter = nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    // Define the email options
+    const mailOptions = {
+        from: `Chato App <${process.env.EMAIL_USERNAME}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message,
+        html: options.html,
+    };
+
+    // Send the email
+    await transporter.sendMail(mailOptions);
+}
+
+module.exports = sendEmail;
