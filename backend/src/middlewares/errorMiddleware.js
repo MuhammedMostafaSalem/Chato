136f96a4408098043f540087c3600e966b8944bf
Marwan Salem <marwansalem164@gmail.com>
2026-01-14 07:35:43 +0200

    Edit variable

--- backend/src/middlewares/errorMiddleware.js
+++ backend/src/middlewares/errorMiddleware.js
@@ -1,8 +1,8 @@
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
+    let statusCode = err.statusCode || 500;
+    let message = err.message || "Internal Server Error";
 
     // Mongoose duplicate key error
     if(err.code === 11000) {
