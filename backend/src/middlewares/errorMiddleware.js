6003ac6df9dcbb202462640e9eb79fd6dff5a979
Marwan Salem <marwansalem164@gmail.com>
2026-01-15 11:10:36 +0200

    Mongoose Cast and JWT error

--- backend/src/middlewares/errorMiddleware.js
+++ backend/src/middlewares/errorMiddleware.js
@@ -5,21 +5,44 @@ module.exports = (err, req, res, next) => {
     let message = err.message || "Internal Server Error";
 
     // Mongoose duplicate key error
-    if(err.code === 11000) {
+    if (err.code === 11000) {
         statusCode = 400;
         const field = Object.keys(err.keyValue)[0];
         message = `The ${field} '${err.keyValue[field]}' is already in use. Please choose another one.`;
 
         err = new ErrorHandler(message, statusCode);
     }
-    
+
     // Mongoose validation error
-    if(err.name === "ValidationError") {
+    if (err.name === "ValidationError") {
         statusCode = 400;
         const messages = Object.values(err.errors).map(value => value.message);
         message = `${messages}`;
 
         err = new ErrorHandler(message, statusCode);
+    }
+
+    // Mongoose Cast error
+    if (err.name === "CastError") {
+        statusCode = 400;
+        message = `Resource not found. Invalid ${err.path}: ${err.value}`;
+
+        err = new ErrorHandler(message, statusCode);
+    }
+
+    // JWT error
+    if (err.name === "JsonWebTokenError") {
+        statusCode = 401;
+        const message = "Invalid token. Please login again.";
+        err = new Errorhandler(message, statusCode);
+    }
+
+    // JWT EXPIRE error
+    if (err.name === "TokenExpiredError") {
+        statusCode = 401;
+        const message = "Token expired. Please login again.";
+
+        err = new Errorhandler(message, statusCode);
     }
 
     console.log(err);
