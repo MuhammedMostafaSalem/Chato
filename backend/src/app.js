0977d3cd70c29580dea148645d02ccaea55f6047
Marwan Salem <marwansalem164@gmail.com>
2026-01-15 09:03:38 +0200

    Add middleware cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
 const jsendMiddleware = require('./middlewares/jsendMiddleware');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
@@ -7,6 +8,7 @@ const app = express();
 const authRoute = require('./routes/authRoute'); // Import auth routes
 
 app.use(express.json());  // Middleware to parse JSON request bodies
+app.use(cookieParser());  // Middleware to cookie parser
 app.use(jsendMiddleware);  // Middleware to add JSend response methods
 
 
