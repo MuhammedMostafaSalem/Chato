0b587cdd0623478d0e65877df07a2857e8db1cde
Marwan Salem <marwansalem164@gmail.com>
2026-01-15 09:02:50 +0200

    Implement complete auth controllers

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,14 +1,18 @@
 const asyncWrapper = require("../middlewares/asyncWrapper");
 const User = require("../models/userModels");
-const otpTemplate = require("../templates/otpTemplate");
 const sendEmail = require("../utils/sendEmail");
 const ErrorHandler = require("../utils/errorHandler");
 const { verifyOtp, resendOtp } = require("../services/otpService");
+const sendToken = require("../services/sendToken");
+const jwt = require("jsonwebtoken");
+const verifyEmailTemplate = require("../templates/verifyEmailTemplate");
 
 // Registration logic will go here
 const register = asyncWrapper(async (req, res, next) => {
+    // Extract user details from request body
     const { username, email, phoneNumber, password } = req.body;
 
+    // add user to database
     const newUser = new User({
         username,
         email,
@@ -17,51 +21,51 @@ const register = asyncWrapper(async (req, res, next) => {
     });
 
     // Generate OTP for email verification
-    const otp = await newUser.generateOtp();
+    const otp = await newUser.generateOtp("email_verification");
     await newUser.save();
 
     // Send verification email with OTP
     await sendEmail({
         email: newUser.email,
-        subject: 'Verify your email for Chato App',
+        subject: 'Verify your email - Chato App',
         message: `Your OTP for email verification is: ${otp}. It is valid for 10 minutes.`,
-        html: otpTemplate(newUser.username, otp),
+        html: verifyEmailTemplate(newUser.username, otp),
     });
 
-    // Respond to the client
+    // Send success response
     res.success('otp sent to your email for verification', 200);
 });
 
-// verify email logic will go here
-const verifyEmailOtp = asyncWrapper(async (req, res, next) => {
+// verify account logic will go here
+const verifyAccountOtp = asyncWrapper(async (req, res, next) => {
     // Extract OTP from request body
-    const {otp} = req.body;
+    const { otp, purpose } = req.body;
 
     // Validate OTP
-    if (!otp) {
-        return next(new ErrorHandler("OTP is required", 400));
+    if (!otp) return next(new ErrorHandler("OTP is required", 400));
+
+    // Verify OTP using service layer
+    const user = await verifyOtp(otp, purpose);
+
+    let message;
+    if (purpose === "email_verification") {
+        message = "User verified successfully";
+    }
+    if (purpose === "forgot_password") {
+        message = "OTP verified, you can reset your password now";
     }
 
-    try {
-        // Verify OTP using service layer
-        const user = await verifyOtp(otp);
-
-        // Send success response
-        res.success({
-            message: "User verified successfully",
-            username: user.username
-        }, 200);
-
-    } catch (error) {
-        // Forward service errors to global error handler
-        return next(new ErrorHandler(error.message, 400));
-    }
+    // Send success response
+    res.success({
+        message,
+        username: user.username
+    }, 200);
 });
 
-// resend email otp logic will go here
-const resendEmailOtp = asyncWrapper(async (req, res, next) => {
+// resend account otp logic will go here
+const resendAccountOtp = asyncWrapper(async (req, res, next) => {
     // Extract phoneNumber from request body
-    const { phoneNumber } = req.body;
+    const { phoneNumber, purpose } = req.body;
 
     // Validate phone number input
     if (!phoneNumber) {
@@ -69,14 +73,130 @@ const resendEmailOtp = asyncWrapper(async (req, res, next) => {
     }
 
     // Resend OTP using service layer
-    await resendOtp(phoneNumber);
+    await resendOtp(phoneNumber, purpose);
+
+    let message;
+    if (purpose === "email_verification") {
+        message = "Verification OTP resent successfully";
+    }
+    if (purpose === "forgot_password") {
+        message = "Password reset OTP resent successfully";
+    }
 
     // Send success response
-    res.success("OTP resent successfully", 200);
+    res.success(message, 200);
+});
+
+// Loging logic will go here
+const login = asyncWrapper(async (req, res, next) => {
+    // Extract phoneNumber and password from request body
+    const { phoneNumber, password } = req.body;
+
+    // Find user by phoneNumber and include password field
+    const user = await User.findOne({ phoneNumber }).select("+password")
+
+    // If user not found, return error
+    if (!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    // Check if user is verified
+    if (!user.isVerified) {
+        return next(new ErrorHandler("Please verify your account first", 403));
+    }
+
+    // Compare provided password with stored hashed password
+    const isMatch = await user.comparePassword(password);
+    // If password does not match, return error
+    if (!isMatch) {
+        return next(new ErrorHandler("Password is incorrect", 400));
+    }
+
+    // If login is successful, send tokens
+    sendToken(user, 200, res)
+});
+
+// Refresh token logic here
+const refreshToken = asyncWrapper(async (req, res, next) => {
+    // Get refresh token from cookies
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+        return next(new ErrorHandler("Please login again", 401));
+    }
+
+    // Verify refresh token
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+
+    // Find user and check if refresh token matches
+    const user = await User.findById(decoded.id);
+    if (!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    // Generate new access token
+    const newAccessToken = user.generateAccessToken();
+
+    // Send new access token
+    res.success({ accessToken: newAccessToken }, 200);
+});
+
+// Forgot password logic here
+const forgotPassword = asyncWrapper(async (req, res, next) => {
+    // Extract phoneNumber from request body
+    const { phoneNumber } = req.body;
+
+    // Resent OTP using service layer
+    await resendOtp(phoneNumber, "forgot_password");
+
+    // Respond to the client
+    res.success('OTP sent to your email for reset password', 200);
+});
+
+// Reset password logic here
+const resetPassword = asyncWrapper(async (req, res, next) => {
+    // Extract phoneNumber, password and confirm password from request body
+    const { phoneNumber, password } = req.body;
+
+    // Find user by phoneNumber
+    const user = await User.findOne({ phoneNumber });
+
+    // If user not found, return error
+    if (!user) return next(new ErrorHandler("User not found", 400));
+
+    // clear password and OTP fields
+    user.password = password;
+    user.otp = undefined;
+    user.otpExpires = undefined;
+
+    await user.save();
+
+    // Send success response
+    res.success("Reset password successfully.", 200);
+});
+
+// Logout logic here
+const logout = asyncWrapper(async (req, res, next) => {
+    // Cookie settings (Security Options)
+    const cookieOptions = {
+        httpOnly: true,
+        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+    }
+
+    // Clear refresh token cookie
+    res.clearCookie("refreshToken", cookieOptions);
+
+    // Send success response
+    res.success("Logged out successfully", 200);
 });
 
 module.exports = {
     register,
-    verifyEmailOtp,
-    resendEmailOtp
+    verifyAccountOtp,
+    resendAccountOtp,
+    login,
+    refreshToken,
+    forgotPassword,
+    resetPassword,
+    logout
 };
