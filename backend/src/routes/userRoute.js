8e0a8c8479610b5b86ba75b38f142220e6d303b4
Marwan Salem <marwansalem164@gmail.com>
2026-02-05 15:54:47 +0200

    Add updateProfile, updateAvatar, deleteAvatar

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const { getUserProfile } = require('../controllers/userController');
+const {
+    getUserProfile,
+    updateProfile,
+    deleteAvatar,
+    updateAvatar
+} = require('../controllers/userController');
 const protectUser = require('../middlewares/authMiddleware');
+const { validateUpdateProfile } = require('../utils/validator/profileValidator');
+const createUploader = require('../middlewares/multerMiddleware');
 
 // Create a new router instance
 const router = express.Router();
 
+// Location of user profile image upload
+const uploadUser = createUploader('upload/users');
+
 // User routes
 router.get('/getMe', protectUser, getUserProfile);
+router.patch(
+    '/updateMe',
+    protectUser,
+    validateUpdateProfile,
+    updateProfile
+);
+router.put(
+    '/updateAvatar',
+    protectUser,
+    uploadUser.single('avatar'),
+    updateAvatar
+);
+router.delete('/deleteAvatar', protectUser, deleteAvatar);
 
 module.exports = router;
